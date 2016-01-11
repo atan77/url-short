ed290fac883774325ad7c2c0cd9f143b056794ed
Ashley <atan77@gmail.com>
2016-01-11 12:04:21 +1100

    separated newMini function from app.post

--- server-old.js
+++ server-old.js
@@ -40,17 +40,41 @@ if(re.test(submitUrl)==false) {
   console.log('no http://');
 var httpSubmitUrl = 'http://' + submitUrl;
 newMini(httpSubmitUrl);
+  //determine the number of urls in the database to assign a sequence id to create the miniurl
+//  miniurl.find({}).count({}, function(err, count) {
+//    console.log('number of docs ' + count);
+  //create the miniurl
+//  miniurl.create({origurl: httpSubmitUrl, seq: count+1, miniurl: 'http://s-u.herokuapp.com/' + (count+1)}, function(err,result) {
+//    console.log('added ' + httpSubmitUrl + ' to the list with sequence: ' + (count+1));
+  //probably not the fastest way to do it, but returns the created miniurl from the database and send it to the user
+//    miniurl.findOne({origurl: httpSubmitUrl}, function (err, result) {
+//      if (err) return console.log(err);
+//      console.log('your short url is: ' + result.miniurl);
+//      res.send('your short url is: ' + result.miniurl)
+//    });
+//  });
+//  });
+
 
 } else {
 console.log('attempting to add: ' + submitUrl)
 newMini(submitUrl);
-
+//determine the number of urls in the database to assign a sequence id to create the miniurl
+//miniurl.find({}).count({}, function(err, count) {
+//  console.log('number of docs ' + count);
+//create the miniurl
+//miniurl.create({origurl: submitUrl, seq: count+1, miniurl: 'http://s-u.herokuapp.com/' + (count+1)}, function(err,result) {
+//  console.log('added ' + submitUrl + ' to the list with sequence: ' + (count+1));
+//probably not the fastest way to do it, but returns the created miniurl from the database and send it to the user
+//  miniurl.findOne({origurl: submitUrl}, function (err, result) {
+//    if (err) return console.log(err);
+//    console.log('your short url is: ' + result.miniurl);
+//    res.send('your short url is: ' + result.miniurl)
+//  });
+//});
+//});
 }
 //***started change to a function to generate new miniurl
-
-
-
-});
 
 function newMini(subUrl) {
   console.log('attempting to add: ' + subUrl)
@@ -70,6 +94,8 @@ function newMini(subUrl) {
   });
   });
 }
+
+});
 
 app.get('/:seq', function(req, res) {
 //get the url submitted
